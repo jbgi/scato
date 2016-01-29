36cf47e06b1583ba1e2ed3236d79b7e4117f45ac
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2016-01-29 20:11:38 +0100

    Fix compile of IOInstances

--- io/src/main/scala/IOInstances.scala
+++ io/src/main/scala/IOInstances.scala
@@ -1,11 +1,11 @@
 package scato
 package io
 
-import scato.clazz.{BindAp, MonadTemplate}
+import scato.clazz.{Monad, Bind}
 
 import IO.syntax.constIO
 
-trait IOInstances extends MonadTemplate[IO] with BindAp[IO] {
+trait IOInstances extends Monad.Template[IO] with Bind.Ap[IO] {
   override def flatMap[A, B](ioa: IO[A])(f: A => IO[B]): IO[B] = ioa.flatMap(f)
   override def map[A, B](ioa: IO[A])(f: A => B): IO[B] = ioa.map(f)
   override def pure[A](a: A): IO[A] = constIO[A](a)
